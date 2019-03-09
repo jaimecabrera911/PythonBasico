6ddf4bc6f71539508fac765f317b6c253f48e1fa
Jaime <cabrerajaimenadres@gmail.com>
2019-03-08 23:44:52 -0500

    Final

--- Textos.py
+++ Textos.py
@@ -1,4 +1,4 @@
-#TEXTOS O STRINGS
+# TEXTOS O STRINGS
 
 print('Hola Mundo comillas simples')
 print("Hola Mundo comillas dobles")
@@ -7,4 +7,13 @@ print("'Texto' entre comillas simples")
 print("Una cadena\nOtra cadena\nOtra cadena")  # Salto de linea dentro de un texto
 print("Tabulacion\tTabulacion\tTabulacion")  # Tabulacion
 print(r"C:\nueva_carpeta")  # imprimir sin caracteres especiales
-
+print("""Esto es otra
+forma de imprimir una
+cadena texto con saltos de linea""")
+nombre = "Jaime"
+apellido = "Cabrera"
+print("Tu nombre es", nombre, apellido)  # Concatenar cadenas de texto
+nombreCompleto = "Jaime" + "Cabrera"
+print(nombreCompleto)
+espacios = " " * 20
+print(espacios, "Espacios")
