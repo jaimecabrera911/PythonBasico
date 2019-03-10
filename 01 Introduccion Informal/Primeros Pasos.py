fc1bf9a14c5465bef7193ebe6aefae93c9a8a6f9
Jaime <cabrerajaimenadres@gmail.com>
2019-03-10 01:31:43 -0500

    Primeros Pasos

---
+++ 01 Introduccion Informal/Primeros Pasos.py
@@ -0,0 +1,8 @@
+n = 0
+while n < 10:
+    n +=1 # Esto seria el elemento iterador que  aumentaria de 1 en  1 hasta 10 esto es igual a n=n+1
+    if (n % 2) == 0:
+        print(n, "par")
+    else:
+        print(n, "impar")
+
