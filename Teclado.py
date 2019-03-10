860350b20349ca203be1b3602b807002b886399e
Jaime <cabrerajaimenadres@gmail.com>
2019-03-10 00:34:47 -0500

    Teclado

---
+++ Teclado.py
@@ -0,0 +1,21 @@
+print("¿Cual es tu nombre?")
+nombre = input()
+print("¿Cual es tu apeliido?")
+apellido = input()
+print("Hola", nombre, apellido)
+
+numero1 = input("Escribe un numero")  # El input asi sea numero arroja un string
+numero2 = input("Escribe otro numero")
+
+print(numero1 + numero2)
+
+numero1 = int(input("Escribe un numero"))  # Si agremamos la funcion int nos convierte el string en entero
+numero2 = int(input("Escribe otro numero"))
+
+print(numero1 + numero2)
+
+decimal1 = float(input("Escribe un decimal o entero"))  # Si agremamos la funcion float nos convierte el string en entero
+decimal2 = float(input("Escribe otro decimal o entero"))
+
+print(decimal1 + decimal2)
+
